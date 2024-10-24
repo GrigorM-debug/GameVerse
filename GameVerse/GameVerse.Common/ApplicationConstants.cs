908747dd32f764c486b02bd144b5aeab528c1ffc
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-24 19:01:40 +0300

    Added class for Application Constants. Added Platform table

--- GameVerse/GameVerse.Common/ApplicationConstants.cs
+++ GameVerse/GameVerse.Common/ApplicationConstants.cs
@@ -2,6 +2,21 @@
 {
     public static class ApplicationConstants
     {
+        //Project start date
         public const int ProjectStartDate = 2024;
+
+        //DateTime format
+        public const string DateTimeFormat = "dd-MM-yyyy";
+
+        //Error Messages
+        public const string LengthErrorMessage = "{0} must be between {1} and {2} characters long!";
+        public const string invalidDateTimeErrorMessage = $"Date must be in the following format - ${DateTimeFormat}";
+
+        public static class PlatformConstants
+        {
+            //Name
+            public const int NameMinLength = 2;
+            public const int NameMaxLength = 30;
+        }
     }
 }
---
+++ GameVerse/GameVerse.Data.Models/Platform.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GameVerse.Common.ApplicationConstants;
+
+namespace GameVerse.Data.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        [Comment("The platform unique identifier.")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(PlatformConstants.NameMaxLength, MinimumLength = PlatformConstants.NameMinLength, ErrorMessage = LengthErrorMessage]
+        public string Name { get; set; } = null!;
+
+        public ICollection<GamePlatform> GamePlatforms { get; set; } = new HashSet<GamePlatform>();
+    }
+}
