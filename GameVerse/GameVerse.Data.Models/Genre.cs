a8d245c1d8fae66a8f32b6d7490289568df3af18
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-24 19:57:26 +0300

    Added Genre Table

---
+++ GameVerse/GameVerse.Data.Models/Genre.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static GameVerse.Common.ApplicationConstants;
+
+namespace GameVerse.Data.Models
+{
+    public class Genre
+    {
+        [Key]
+        [Required]
+        [Comment("The genre unique indetifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(GenreConstants.NameMaxlength, MinimumLength = GenreConstants.NameMinLength, ErrorMessage = LengthErrorMessage)]
+        [Comment("The name of the genre")]
+        public string Name { get; set; } = null!;
+
+        public ICollection<Game> Games { get; set; } = new HashSet<Game>();
+    }
+}
