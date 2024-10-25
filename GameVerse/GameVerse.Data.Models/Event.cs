ed94e716ab91900b05abbf01a420dcdd49238317
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-25 09:46:51 +0300

    Added Event Table

---
+++ GameVerse/GameVerse.Data.Models/Event.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameVerse.Data.Models
+{
+    public class Event
+    {
+        [Key]
+        [Required]
+        [Comment("The event unique identifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Comment("The topic of the event")]
+        public string Topic { get; set; } = null!;
+
+        [Required]
+        [Comment("The description of the event")]
+        public string Description { get; set; } = null!;
+
+        [Comment("The event starting date and time")]
+        public DateTime StartDate { get; set; }
+
+        [Comment("The event end date and time")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [Comment("The event location")]
+        public string Location { get; set; } = null!;
+
+        [Comment("The event's number of seats")]
+        public int Seats { get; set; }
+
+        [Comment("The price for ticket")]
+        public decimal TicketPrice { get; set; }
+
+        [Required]
+        [Comment("The event image url")]
+        public string ImageUrl { get; set; } = null!;
+
+        
+    }
+}
