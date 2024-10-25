3fdb6b4ca2ba65576c778138a0c1f5820d963a31
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-25 09:34:45 +0300

    Added GameCart mapping table

---
+++ GameVerse/GameVerse.Data.Models/GameCart.cs
@@ -0,0 +1,18 @@
+﻿using GameVerse.Data.Models.Games;
+
+namespace GameVerse.Data.Models
+{
+    public class GameCart
+    {
+        public Guid CartId { get; set; }
+
+        public Cart Cart { get; set; } = null!;
+
+        public Guid GameId { get; set; }
+        public Game Game { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
