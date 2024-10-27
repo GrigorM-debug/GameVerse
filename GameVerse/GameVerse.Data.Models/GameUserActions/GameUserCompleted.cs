4ad622b782f3351b5ffc0a2c1d5a97ed83b2dec5
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-27 20:25:22 +0200

    Added ApplicationUser properties in GameUserCompleted and GameUserCurrentlyPlaying tables

--- GameVerse/GameVerse.Data.Models/GameUserActions/GameUserCompleted.cs
+++ GameVerse/GameVerse.Data.Models/GameUserActions/GameUserCompleted.cs
@@ -1,16 +1,26 @@
 ﻿using GameVerse.Data.Models.Games;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameVerse.Data.Models.GameUserActions
 {
+    [PrimaryKey(nameof(GameId), nameof(UserId))]
     public class GameUserCompleted
     {
         [Comment("The id of the Game")]
         public Guid GameId { get; set; }
 
+        [Required]
+        [ForeignKey(nameof(GameId))]
         public Game Game { get; set; } = null!;
 
-        //Add properties for Application user
+        [Comment("The id of the User")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
 
         [Comment("The date and time when the game is completed")]
         public DateTime CompletedOn { get; set; }
--- GameVerse/GameVerse.Data.Models/GameUserActions/GameUserCurrentlyPlaying.cs
+++ GameVerse/GameVerse.Data.Models/GameUserActions/GameUserCurrentlyPlaying.cs
@@ -1,15 +1,28 @@
 ﻿using GameVerse.Data.Models.Games;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameVerse.Data.Models.GameUserActions
 {
+    [PrimaryKey(nameof(GameId), nameof(UserId))]
     public class GameUserCurrentlyPlaying
     {
+        [Comment("The id of the game")]
         public Guid GameId { get; set; }
 
+        [Required]
+        [ForeignKey(nameof(GameId))]
         public Game Game { get; set; } = null!;
+
+        [Comment("The id of the user")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
 
         public DateTime AddedOn { get; set; }
 
-        // Add properties for ApplicationUser
     }
 }
