062785210bdd4b22f2fba07945e131fdb139a697
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-27 19:31:41 +0200

    Indetity Scaffold

--- GameVerse/GameVerse.Data/GameVerseContext.cs
+++ GameVerse/GameVerse.Data/GameVerseContext.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GameVerse.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GameVerse.Data
 {
-    public class GameVerseContext : IdentityDbContext
+    public class GameVerseContext : IdentityDbContext<ApplicationUser>
     { 
         public GameVerseContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
