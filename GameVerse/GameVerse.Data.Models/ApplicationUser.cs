e42752b5f0be6afdf5777468cbba107e30d218fd
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-27 20:54:24 +0200

    Some cleanup in ApplicationUser Model

--- GameVerse/GameVerse.Data.Models/ApplicationUser.cs
+++ GameVerse/GameVerse.Data.Models/ApplicationUser.cs
@@ -16,19 +16,19 @@ namespace GameVerse.Data.Models
 
         [PersonalData]
         [Required]
-        [StringLength(ApplicationUserConstants.ApplicationUserUserNameMaxLength, MinimumLength = ApplicationUserConstants.ApplicationUserUserNameMinLength, ErrorMessage = ApplicationConstants.LengthErrorMessage)]
+        [StringLength(ApplicationUserConstants.ApplicationUserUserNameMaxLength, MinimumLength = ApplicationUserConstants.ApplicationUserUserNameMinLength, ErrorMessage = LengthErrorMessage)]
         [Comment("The username of the ApplicationUser")]
         public string Username { get; set; } = null!;
 
         [PersonalData]
         [Required]
-        [StringLength(ApplicationUserConstants.ApplicationUserFirstNameMaxLength, MinimumLength = ApplicationUserConstants.ApplicationUserFirtNameMinLength, ErrorMessage = ApplicationConstants.LengthErrorMessage)]
+        [StringLength(ApplicationUserConstants.ApplicationUserFirstNameMaxLength, MinimumLength = ApplicationUserConstants.ApplicationUserFirtNameMinLength, ErrorMessage = LengthErrorMessage)]
         [Comment("The FirstName of the ApplicationUser")]
         public string FirstName { get; set; } = null!;
 
         [PersonalData]
         [Required]
-        [StringLength(ApplicationUserConstants.ApplicationUserLastNameMaxLength, MinimumLength = ApplicationUserConstants.ApplicationUserLastNameMinLength, ErrorMessage = ApplicationConstants.LengthErrorMessage)]
+        [StringLength(ApplicationUserConstants.ApplicationUserLastNameMaxLength, MinimumLength = ApplicationUserConstants.ApplicationUserLastNameMinLength, ErrorMessage = LengthErrorMessage)]
         [Comment("The LastName of the ApplicationUser")]
         public string LastName { get; set; } = null!;
 
