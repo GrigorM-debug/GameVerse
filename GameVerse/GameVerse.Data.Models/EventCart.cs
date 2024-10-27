e369fa9046013f2708f06dcff59d9a10c1fb92c5
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-27 20:38:25 +0200

    Added Composite keys in Event Cart and Game Cart tables

--- GameVerse/GameVerse.Data.Models/EventCart.cs
+++ GameVerse/GameVerse.Data.Models/EventCart.cs
@@ -1,20 +1,28 @@
 ﻿
 using GameVerse.Data.Models.Events;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameVerse.Data.Models
 {
+    [PrimaryKey(nameof(EventId), nameof(CartId))]
     public class EventCart
     {
-        public Guid CartId { get; set; }
-
-        public Cart Cart { get; set; } = null!;
-
         public Guid EventId { get; set; }
 
+        [Required]
+        [ForeignKey(nameof(EventId))]
         public Event Event { get; set; } = null!;
+
+        public Guid CartId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CartId))]
+        public Cart Cart { get; set; } = null!;
 
         public int TicketQuantity { get; set; }
 
-        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
     }
 }
--- GameVerse/GameVerse.Data.Models/GameCart.cs
+++ GameVerse/GameVerse.Data.Models/GameCart.cs
@@ -1,18 +1,27 @@
 ﻿using GameVerse.Data.Models.Games;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameVerse.Data.Models
 {
+    [PrimaryKey(nameof(GameId), nameof(CartId))]
     public class GameCart
     {
+        public Guid GameId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(GameId))]
+        public Game Game { get; set; } = null!;
+
         public Guid CartId { get; set; }
 
+        [Required]
+        [ForeignKey(nameof(CartId))]
         public Cart Cart { get; set; } = null!;
-
-        public Guid GameId { get; set; }
-        public Game Game { get; set; } = null!;
 
         public int Quantity { get; set; }
 
-        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
     }
 }
