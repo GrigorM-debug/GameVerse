1215292de202ee5272cbc15edb00f7dbc329f1ee
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-27 20:52:19 +0200

    Added Composite Key and ApplicationUser properties in EventRegistrations table

--- GameVerse/GameVerse.Data.Models/Events/EventRegistration.cs
+++ GameVerse/GameVerse.Data.Models/Events/EventRegistration.cs
@@ -1,13 +1,24 @@
-﻿namespace GameVerse.Data.Models.Events
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameVerse.Data.Models.Events
 {
+    [PrimaryKey(nameof(EventId), nameof(UserId))]
     public class EventRegistration
     {
         public Guid EventId { get; set; }
 
+        [Required]
+        [ForeignKey(nameof(EventId))]
         public Event Event { get; set; } = null!;
 
-        //Add Application User properties
+        public Guid UserId { get; set; }
 
+        [Required]
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+        
         public DateTime RegistrationDate { get; set; }
     }
 }
