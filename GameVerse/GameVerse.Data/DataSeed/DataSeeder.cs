5b0def39812d6a6a7ee8d404cac4a2f8cd5482f9
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-28 19:53:08 +0200

    Added DataSeeder class

---
+++ GameVerse/GameVerse.Data/DataSeed/DataSeeder.cs
@@ -0,0 +1,72 @@
+﻿using GameVerse.Data.Models.Games.Genres;
+using GameVerse.Data.Models.Games.Platform;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameVerse.Data.DataSeed
+{
+    internal static class DataSeeder
+    {
+        public static void SeedGenres(ModelBuilder modelBuilder)
+        {
+            List<Genre> genres = new List<Genre>()
+            {
+                new Genre { Id = Guid.NewGuid(), Name = "Adventure"},
+                new Genre { Id = Guid.NewGuid(), Name = "Action"},
+                new Genre { Id = Guid.NewGuid(), Name = "Sports"},
+                new Genre { Id = Guid.NewGuid(), Name = "Simulation" },
+                new Genre { Id = Guid.NewGuid(), Name = "Platformer" },
+                new Genre { Id = Guid.NewGuid(), Name = "RPG" },
+                new Genre { Id = Guid.NewGuid(), Name = "First-Person Shooter" },
+                new Genre { Id = Guid.NewGuid(), Name = "Action-adventure" },
+                new Genre { Id = Guid.NewGuid(), Name = "Fighting" },
+                new Genre { Id = Guid.NewGuid(), Name = "Real-time strategy" },
+                new Genre { Id = Guid.NewGuid(), Name = "Racing" },
+                new Genre { Id = Guid.NewGuid(), Name = "Shooter" },
+                new Genre { Id = Guid.NewGuid(), Name = "Puzzle" },
+                new Genre { Id = Guid.NewGuid(), Name = "Casual" },
+                new Genre { Id = Guid.NewGuid(), Name = "Strategy game" },
+                new Genre { Id = Guid.NewGuid(), Name = "Stealth" },
+                new Genre { Id = Guid.NewGuid(), Name = "Party" },
+                new Genre { Id = Guid.NewGuid(), Name = "Action RPG" },
+                new Genre { Id = Guid.NewGuid(), Name = "Survival" },
+                new Genre { Id = Guid.NewGuid(), Name = "Battle Royale" }
+            };
+
+            modelBuilder.Entity<Genre>().HasData(genres);
+        }
+
+        public static void SeedPlatforms(ModelBuilder modelBuilder)
+        {
+            List<Platform> platforms = new List<Platform>()
+            {
+                new Platform { Id = Guid.NewGuid(), Name = "PC" },
+                new Platform { Id = Guid.NewGuid(), Name = "Mac"},
+                new Platform { Id = Guid.NewGuid(), Name = "PlayStation 1"},
+                new Platform { Id = Guid.NewGuid(), Name = "PlayStation 2" },
+                new Platform { Id = Guid.NewGuid(), Name = "PlayStation 3"},
+                new Platform { Id = Guid.NewGuid(), Name = "PlayStation 4"},
+                new Platform { Id = Guid.NewGuid(), Name = "PlayStation 5"},
+                new Platform { Id = Guid.NewGuid(), Name = "Xbox One"},
+                new Platform { Id = Guid.NewGuid(), Name = "Linux"},
+                new Platform { Id = Guid.NewGuid(), Name = "Nintendo Switch"},
+                new Platform { Id = Guid.NewGuid(), Name = "Nintendo Wii" },
+                new Platform { Id = Guid.NewGuid(), Name = "Xbox Series X"},
+                new Platform { Id = Guid.NewGuid(), Name = "Xbox 360"},
+                new Platform { Id = Guid.NewGuid(), Name = "Nintendo DS"},
+                new Platform { Id = Guid.NewGuid(), Name = "Super Nintendo" },
+                new Platform { Id = Guid.NewGuid(), Name = "PlayStation Portable" },
+                new Platform { Id = Guid.NewGuid(), Name = "PlayStation Vita" },
+                new Platform { Id = Guid.NewGuid(), Name = "Game Boy" },
+                new Platform { Id = Guid.NewGuid(), Name = "Game Boy Advanced" },
+                new Platform { Id = Guid.NewGuid(), Name = "Sega Mega Drive" }
+            };
+
+            modelBuilder.Entity<Platform>().HasData(platforms);
+        }
+    }
+}
--- GameVerse/GameVerse.Data/GameVerseContext.cs
+++ GameVerse/GameVerse.Data/GameVerseContext.cs
@@ -1,4 +1,5 @@
-﻿using GameVerse.Data.Models;
+﻿using GameVerse.Data.DataSeed;
+using GameVerse.Data.Models;
 using GameVerse.Data.Models.Games;
 using GameVerse.Data.Models.Games.Genres;
 using GameVerse.Data.Models.Games.Platform;
@@ -16,55 +17,9 @@ namespace GameVerse.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder
-                .Entity<Platform>()
-                .HasData(
-                    new Platform { Id = Guid.NewGuid(), Name = "PC" },
-                    new Platform { Id = Guid.NewGuid(), Name = "Mac"},
-                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 1"}, 
-                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 2" },
-                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 3"},
-                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 4"},
-                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 5"},
-                    new Platform { Id = Guid.NewGuid(), Name = "Xbox One"},
-                    new Platform { Id = Guid.NewGuid(), Name = "Linux"},
-                    new Platform { Id = Guid.NewGuid(), Name = "Nintendo Switch"},
-                    new Platform { Id = Guid.NewGuid(), Name = "Nintendo Wii" },
-                    new Platform { Id = Guid.NewGuid(), Name = "Xbox Series X"},
-                    new Platform { Id = Guid.NewGuid(), Name = "Xbox 360"},
-                    new Platform { Id = Guid.NewGuid(), Name = "Nintendo DS"},
-                    new Platform { Id = Guid.NewGuid(), Name = "Super Nintendo" },
-                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation Portable" },
-                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation Vita" },
-                    new Platform { Id = Guid.NewGuid(), Name = "Game Boy" },
-                    new Platform { Id = Guid.NewGuid(), Name = "Game Boy Advanced" },
-                    new Platform { Id = Guid.NewGuid(), Name = "Sega Mega Drive" }
-                );
 
-            builder
-                .Entity<Genre>()
-                .HasData(
-                    new Genre { Id = Guid.NewGuid(), Name = "Adventure"},
-                    new Genre { Id = Guid.NewGuid(), Name = "Action"},
-                    new Genre { Id = Guid.NewGuid(), Name = "Sports"},
-                    new Genre { Id = Guid.NewGuid(), Name = "Simulation" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Platformer" },
-                    new Genre { Id = Guid.NewGuid(), Name = "RPG" },
-                    new Genre { Id = Guid.NewGuid(), Name = "First-Person Shooter" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Action-adventure" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Fighting" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Real-time strategy" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Racing" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Shooter" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Puzzle" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Casual" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Strategy game" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Stealth" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Party" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Action RPG" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Survival" },
-                    new Genre { Id = Guid.NewGuid(), Name = "Battle Royale" }
-                );
+            DataSeeder.SeedGenres(builder);
+            DataSeeder.SeedPlatforms(builder);
 
             builder
                 .Entity<Game>()
