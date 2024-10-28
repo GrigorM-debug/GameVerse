2a3e3656f4c7815cee46cf19e2e713a6b1d65e69
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-28 18:48:12 +0200

    Added Configuration for converting Enum to string in the Db

--- GameVerse/GameVerse.Data/GameVerseContext.cs
+++ GameVerse/GameVerse.Data/GameVerseContext.cs
@@ -1,4 +1,5 @@
 ﻿using GameVerse.Data.Models;
+using GameVerse.Data.Models.Games;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,11 @@ namespace GameVerse.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder
+                .Entity<Game>()
+                .Property(g => g.Type)
+                .HasConversion<string>(); // Convert Enum to string in the Database. 
+
             base.OnModelCreating(builder);
         }
     }
