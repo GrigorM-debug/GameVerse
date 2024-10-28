294d6d7a8052bee048ba7c852af357752b4ca393
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-28 19:28:18 +0200

    Added Platforms data seed

--- GameVerse/GameVerse.Data/GameVerseContext.cs
+++ GameVerse/GameVerse.Data/GameVerseContext.cs
@@ -1,5 +1,7 @@
 ﻿using GameVerse.Data.Models;
 using GameVerse.Data.Models.Games;
+using GameVerse.Data.Models.Games.Genres;
+using GameVerse.Data.Models.Games.Platform;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +16,31 @@ namespace GameVerse.Data
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder
+                .Entity<Platform>()
+                .HasData(
+                    new Platform { Id = Guid.NewGuid(), Name = "PC" },
+                    new Platform { Id = Guid.NewGuid(), Name = "Mac"},
+                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 1"}, 
+                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 2" },
+                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 3"},
+                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 4"},
+                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation 5"},
+                    new Platform { Id = Guid.NewGuid(), Name = "Xbox One"},
+                    new Platform { Id = Guid.NewGuid(), Name = "Linux"},
+                    new Platform { Id = Guid.NewGuid(), Name = "Nintendo Switch"},
+                    new Platform { Id = Guid.NewGuid(), Name = "Nintendo Wii" },
+                    new Platform { Id = Guid.NewGuid(), Name = "Xbox Series X"},
+                    new Platform { Id = Guid.NewGuid(), Name = "Xbox 360"},
+                    new Platform { Id = Guid.NewGuid(), Name = "Nintendo DS"},
+                    new Platform { Id = Guid.NewGuid(), Name = "Super Nintendo" },
+                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation Portable" },
+                    new Platform { Id = Guid.NewGuid(), Name = "PlayStation Vita" },
+                    new Platform { Id = Guid.NewGuid(), Name = "Game Boy" },
+                    new Platform { Id = Guid.NewGuid(), Name = "Game Boy Advanced" },
+                    new Platform { Id = Guid.NewGuid(), Name = "Sega Mega Drive" }
+                );
+
             builder
                 .Entity<Game>()
                 .Property(g => g.Type)
