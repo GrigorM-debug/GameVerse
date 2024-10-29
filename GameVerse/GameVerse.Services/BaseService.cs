28dbe0ccabff8b3e7c2a4b131bff046ae3d84efa
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-29 22:34:40 +0200

    Added BaseService with method for Guid Id Validation

---
+++ GameVerse/GameVerse.Services/BaseService.cs
@@ -0,0 +1,17 @@
+﻿
+using GameVerse.Services.Interfaces;
+
+namespace GameVerse.Services
+{
+    public class BaseService : IBaseService
+    {
+        public bool IsGuidValid(string? id, ref Guid guidId)
+        {
+            if(String.IsNullOrEmpty(id)) return false;
+
+            if(!Guid.TryParse(id, out guidId)) return false;
+
+            return true;
+        }
+    }
+}
