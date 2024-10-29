ecebfdc738d712f33298efa2521dc236d9f1b956
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-29 21:30:46 +0200

    Added IGenericRepository Interface

---
+++ GameVerse/GameVerse.Data/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+
+namespace GameVerse.Data.Repositories.Interfaces
+{
+    public interface IGenericRepository<TType, TId> 
+    {
+        TType GetById(TId id);
+
+        Task<TType> GetByIdAsync(TId id);
+
+        TType FirstOrDefault(Func<TType, bool> predicate);
+
+        Task<TType> FirstOrDefaultAsync(Expression<Func<TType, bool>> predicate);
+
+        IEnumerable<TType> GetAllAsReadOnly();
+
+        Task<IEnumerable<TType>> GetAllAsyncAsReadOnly();
+
+        IQueryable<TType> GetAllAttached();
+
+        void Add(TType entity);
+
+        Task AddAsync(TType entity);
+
+        void AddRange(TType[] entities);
+
+        Task AddRangeAsync(TType[] entities);
+
+        bool Delete(TId id);
+
+        Task<bool> DeleteAsync(TId id);
+
+        bool Update(TType entity);
+
+        Task<bool> UpdateAsync(TType entity);
+    }
+}
