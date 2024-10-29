a5f3b1aca68f8cdcb45eb52f2bd97a6e1001eaad
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-29 22:52:23 +0200

    Added GameDetailsViewModel

---
+++ GameVerse/GameVerse.Web.ViewModels/Game/GameDetailsViewModel.cs
@@ -0,0 +1,38 @@
+﻿
+using GameVerse.Web.ViewModels.Game.Platforms;
+using GameVerse.Web.ViewModels.Game.Restrictions;
+using GameVerse.Web.ViewModels.Game.Reviews;
+
+namespace GameVerse.Web.ViewModels.Game
+{
+    public class GameDetailsViewModel
+    {
+        public string Id { get; set; } = null!; 
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string PublishingStudio { get; set; } = null!;
+
+        public int YearPublished { get; set; }
+
+        public string CreatedOn { get; set; } = null!;
+
+        public string Price { get; set; } = null!;
+
+        public string Image { get; set; } = null!;
+
+        public int QuantityInStock { get; set; }
+
+        public string Type { get; set; } = null!;
+
+        public string Publisher { get; set; } = null!;
+
+        public IEnumerable<GamePlatformsIndexViewModel> Platforms = new HashSet<GamePlatformsIndexViewModel>();
+        
+        public IEnumerable<GameRestrictionsIndexViewModel> Restrictions = new HashSet<GameRestrictionsIndexViewModel>();
+
+        public IEnumerable<GameReviewsIndexViewModel> Reviews = new HashSet<GameReviewsIndexViewModel>();
+    }
+}
