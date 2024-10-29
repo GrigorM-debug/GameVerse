75a0b5faa2b6781007b4f0d2bab6e31ae0321dcb
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-29 22:39:36 +0200

    Added GameIndexViewModel

---
+++ GameVerse/GameVerse.Web.ViewModels/Game/GameIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace GameVerse.Web.ViewModels.Game
+{
+    public class GameIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string CreatedOn { get; set; } = null!;
+
+        public string Price { get; set; } = null!;
+
+        public string Image {  get; set; } = null!;
+
+        public int QuantityInStock { get; set; }
+
+        public string Publisher { get; set; } = null!;
+    }
+}
