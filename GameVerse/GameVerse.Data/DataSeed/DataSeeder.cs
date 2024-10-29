f00432e0e5d564bae9015e10e74d9f4968cbd886
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-29 19:01:19 +0200

    Removed unused usings in DataSeeder class

--- GameVerse/GameVerse.Data/DataSeed/DataSeeder.cs
+++ GameVerse/GameVerse.Data/DataSeed/DataSeeder.cs
@@ -2,11 +2,6 @@
 using GameVerse.Data.Models.Games.Platform;
 using GameVerse.Data.Models.Games.Restrictions;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GameVerse.Data.DataSeed
 {
