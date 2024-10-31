aa23bb8337d13b1c5dd4089354f7fdc0b6acaf7a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-31 18:32:23 +0200

    Updated EventDetailsViewModel

--- GameVerse/GameVerse.Web.ViewModels/Event/EventDetailsViewModel.cs
+++ GameVerse/GameVerse.Web.ViewModels/Event/EventDetailsViewModel.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace GameVerse.Web.ViewModels.Event
 {
     public class EventDetailsViewModel
     {
+        public string Id { get; set; } = null!;
+
+        public string Topic { get; set; } = null!;
+
+        public string StartDate { get; set; } = null!;
+
+        public string EndDate { get; set; } = null!;
+
+        public int Seats { get; set; }
+
+        public string TicketPrice {  get; set; } = null!;
+
+        public string Image {  get; set; } = null!;
     }
 }
