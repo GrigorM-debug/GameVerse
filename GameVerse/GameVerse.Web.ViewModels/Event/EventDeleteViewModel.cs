935b222ee645a60662216ed4dcb5720b3cfb24c7
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-31 20:05:56 +0200

    Added EventDeleteViewModel

---
+++ GameVerse/GameVerse.Web.ViewModels/Event/EventDeleteViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace GameVerse.Web.ViewModels.Event
+{
+    public class EventDeleteViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Topic { get; set; } = null!;
+
+        public string PublisherId { get; set; } = null!;
+
+        public string PublisherName { get; set;} = null!;
+    }
+}
