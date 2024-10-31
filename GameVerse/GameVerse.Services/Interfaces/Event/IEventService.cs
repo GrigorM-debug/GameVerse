2b0e57da7062b9b8e90e4cef42ea36562ff3bfca
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-10-31 17:31:43 +0200

    Added IEventService Interface and added some of the methods in it

---
+++ GameVerse/GameVerse.Services/Interfaces/Event/IEventService.cs
@@ -0,0 +1,14 @@
+﻿
+using GameVerse.Web.ViewModels.Event;
+
+namespace GameVerse.Services.Interfaces.Event
+{
+    public interface IEventService
+    {
+        Task<IEnumerable<EventIndexViewModel>> GetAllEventsAsync();
+
+        Task<bool> AddEventAsync(EventIndexViewModel inputModel);
+
+        Task<EventDetailsViewModel> GetEventDetailsByIdAsync(Guid id);
+    }
+}
