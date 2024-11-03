7042d4e763568c240ccc40796a8e74b0bc388450
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-03 03:47:37 +0200

    Made HomeViewModel

---
+++ GameVerse/GameVerse.Web.ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+
+using GameVerse.Web.ViewModels.Event;
+
+namespace GameVerse.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<EventIndexViewModel> Last3Events = new HashSet<EventIndexViewModel>();
+    }
+}
