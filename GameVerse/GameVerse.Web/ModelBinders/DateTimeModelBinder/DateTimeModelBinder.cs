11112586f774f681f677d4b7b2035ea671f60eec
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-03 02:24:00 +0200

    Added Xml comments documentation in DateTimeModelBinder and DateTimeModelBinderProvider classes

--- GameVerse/GameVerse.Web/ModelBinders/DateTimeModelBinder/DateTimeModelBinder.cs
+++ GameVerse/GameVerse.Web/ModelBinders/DateTimeModelBinder/DateTimeModelBinder.cs
@@ -5,11 +5,24 @@ using System.Threading.Tasks;
 
 namespace GameVerse.Web.ModelBinders.DateTimeModelBinder
 {
+    /// <summary>
+    /// A custom model binder for binding <see cref="DateTime"/> values from the request using specified date formats.
+    /// </summary>
+    /// <remarks>
+    /// This model binder attempts to parse a date string from the request using an array of date formats.
+    /// If successful, it binds the date value to the model. If parsing fails, it adds a model error to the model state.
+    /// </remarks>
     public class DateTimeModelBinder(string[] dateFormats) : IModelBinder
     {
         private readonly string[] _dateFormats = dateFormats;
         //private readonly CultureInfo? _culture = culture;
 
+        /// <summary>
+        /// Binds the model asynchronously by parsing the date string from the request.
+        /// </summary>
+        /// <param name="bindingContext">The context for model binding, containing the request data.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="bindingContext"/> is <c>null</c>.</exception>
         public Task BindModelAsync(ModelBindingContext bindingContext)
         {
             if (bindingContext == null)
--- GameVerse/GameVerse.Web/ModelBinders/DateTimeModelBinder/DateTimeModelBinderProvider.cs
+++ GameVerse/GameVerse.Web/ModelBinders/DateTimeModelBinder/DateTimeModelBinderProvider.cs
@@ -3,10 +3,28 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
 namespace GameVerse.Web.ModelBinders.DateTimeModelBinder
 {
+    /// <summary>
+    /// A custom model binder provider that creates instances of <see cref="DateTimeModelBinder"/> 
+    /// for binding <see cref="DateTime"/> and <see cref="Nullable{DateTime}"/> model types.
+    /// </summary>
+    /// <remarks>
+    /// This provider checks the model type in the provided context and returns a new instance 
+    /// of the <see cref="DateTimeModelBinder"/> if the model type is <see cref="DateTime"/> 
+    /// or <see cref="Nullable{DateTime}"/>.
+    /// </remarks>
     public class DateTimeModelBinderProvider(string[] dateFormats) : IModelBinderProvider
     {
         private readonly string[] _dateFormats = dateFormats;
 
+        /// <summary>
+        /// Gets the binder for the specified context.
+        /// </summary>
+        /// <param name="context">The context for the model binder provider, which contains metadata about the model type.</param>
+        /// <returns>
+        /// An <see cref="IModelBinder"/> instance if the model type is <see cref="DateTime"/> or <see cref="Nullable{DateTime}"/>; 
+        /// otherwise, <c>null</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="context"/> is <c>null</c>.</exception>
         public IModelBinder? GetBinder(ModelBinderProviderContext context)
         {
             if (context == null)
