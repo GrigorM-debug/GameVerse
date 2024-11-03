90d95850a8f5761d5e7d711be296813724efb22b
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-03 16:14:22 +0200

    Added AllEventQueryModel Because i need it for pagination

---
+++ GameVerse/GameVerse.Web.ViewModels/Event/AllEventsQueryModel.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace GameVerse.Web.ViewModels.Event
+{
+    public class AllEventsQueryModel
+    {
+        public int CurrentPage { get; set; } = 1;
+        public int EventsPerPage { get; set; } = 6;
+
+        public int TotalEventsCount { get; set; }
+
+        public IEnumerable<EventIndexViewModel> Events { get; set; }
+    }
+}
