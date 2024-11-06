1c122e3cd92bea46b3c7f58b0672984988fed5fa
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-07 00:41:01 +0200

    Added GameDeleteViewModel

---
+++ GameVerse/GameVerse.Web.ViewModels/Game/GameDeleteViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameVerse.Web.ViewModels.Game
+{
+    public class GameDeleteViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string PublisherId { get; set; } = null!;
+
+        public string PublisherName { get; set;} = null!;
+    }
+}
