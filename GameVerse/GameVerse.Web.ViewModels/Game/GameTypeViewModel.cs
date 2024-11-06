0b930557e2dbf0df79663eaeede05df8e8133ef4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-06 23:34:48 +0200

    Added GameTypeViewModel

---
+++ GameVerse/GameVerse.Web.ViewModels/Game/GameTypeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameVerse.Web.ViewModels.Game
+{
+    public class GameTypeViewModel
+    {
+        public int Value { get; set; }
+
+        public string Text { get; set; } = null!;
+    }
+}
