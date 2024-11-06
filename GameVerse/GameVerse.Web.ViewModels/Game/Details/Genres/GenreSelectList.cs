99eb12e046e48ebf5a399ac950cf2edf5cfc18c8
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-06 23:35:33 +0200

    Added GenreSelectList, PlatformSelectList, RestrictionSelectList

---
+++ GameVerse/GameVerse.Web.ViewModels/Game/Details/Genres/GenreSelectList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameVerse.Web.ViewModels.Game.Details.Genres
+{
+    public class GenreSelectList
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
