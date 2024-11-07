6134b425af1b303c5c545ae2c648ddc00360421d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-07 23:57:20 +0200

    Added Data Anotation Attributes in GameInputViewModel

--- GameVerse/GameVerse.Web.ViewModels/Game/Add/GameInputViewModel.cs
+++ GameVerse/GameVerse.Web.ViewModels/Game/Add/GameInputViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GameVerse.Common.Enums;
+using Microsoft.VisualBasic;
 using static GameVerse.Common.ApplicationConstants;
 using static GameVerse.Common.ApplicationConstants.GameConstants;
 
@@ -12,22 +14,38 @@ namespace GameVerse.Web.ViewModels.Game.Add
 {
     public class GameInputViewModel
     {
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength, ErrorMessage = LengthErrorMessage)]
         public string Title { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, ErrorMessage = LengthErrorMessage)]
         public string Description { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(PublishingStudioMaxLength, MinimumLength = PublishingStudioMinLength, ErrorMessage = LengthErrorMessage)]
         public string PublishingStudio { get; set; } = string.Empty;
 
+        [Required]
+        [Range(YearPublishedMinValue, YearPublishedMaxValue, ErrorMessage = RangeErrorMessage)]
         public int YearPublished { get; set; }
 
+        [Required]
         public string CreatedOn { get; set; } = DateTime.Now.ToString(DateTimeFormat, CultureInfo.InvariantCulture);
 
+        [Required]
+        [Range(PriceMinValue, PriceMaxValue, ErrorMessage = RangeErrorMessage)]
         public decimal Price { get; set; }
 
+        [Required]
         public string Image { get; set; } = string.Empty;
 
+        [Required]
+        [Range(QuantityInStockMinValue, QuantityInStockMaxValue, ErrorMessage = RangeErrorMessage)]
         public int QuantityInStock { get; set; }
 
+        [Required]
+        [EnumDataType(typeof(GameType), ErrorMessage = InvalidGameTypeErrorMessage)]
         public GameType Type { get; set; }
 
         public IEnumerable<Guid> SelectedGenres { get; set; } = new HashSet<Guid>();
