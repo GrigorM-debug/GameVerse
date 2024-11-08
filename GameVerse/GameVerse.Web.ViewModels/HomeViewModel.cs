fc587b3d0b71363a7dae46b9a055f7642f9cc150
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-08 20:57:38 +0200

    Added Last3Games Collection in HomeViewModel

--- GameVerse/GameVerse.Web.ViewModels/HomeViewModel.cs
+++ GameVerse/GameVerse.Web.ViewModels/HomeViewModel.cs
@@ -1,11 +1,14 @@
 ﻿
 
 using GameVerse.Web.ViewModels.Event;
+using GameVerse.Web.ViewModels.Game;
 
 namespace GameVerse.Web.ViewModels
 {
     public class HomeViewModel
     {
         public IEnumerable<EventIndexViewModel> Last3Events = new HashSet<EventIndexViewModel>();
+
+        public IEnumerable<GameIndexViewModel> Last3Games = new HashSet<GameIndexViewModel>();
     }
 }
