e518faccb988205433081bc6a4ae32565d3b615d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-09 17:21:44 +0200

    Added AllGamesQueryModel

---
+++ GameVerse/GameVerse.Web.ViewModels/Game/AllGamesQueryModel.cs
@@ -0,0 +1,21 @@
+﻿
+
+using GameVerse.Common.Enums;
+
+namespace GameVerse.Web.ViewModels.Game
+{
+    public class AllGamesQueryModel
+    {
+        public int CurrentPage { get; set; } = 1;
+
+        public int GamesPerPage { get; set; } = 6;
+
+        public int TotalGamesCount { get; set; }
+
+        public string? SearchString { get; set; } = string.Empty;
+
+        public EntitySortOrder GameSelectedSortOrder { get; set; } = EntitySortOrder.Newest;
+
+        public IEnumerable<GameIndexViewModel> Games { get; set; } = new HashSet<GameIndexViewModel>();
+    }
+}
