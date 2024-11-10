0ff0ce5f18523d17244ec93ce9bf16aefffbd55c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-10 19:52:09 +0200

    Implement ReviewAlreadyExistByGameIdAndUserIdAsync method in ReviewService

--- GameVerse/GameVerse.Services/ReviewService.cs
+++ GameVerse/GameVerse.Services/ReviewService.cs
@@ -1,13 +1,27 @@
 ﻿
+using GameVerse.Data.Models.Games.Review;
+using GameVerse.Data.Repositories.Interfaces;
 using GameVerse.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameVerse.Services
 {
-    public class ReviewService : IReviewService
+    public class ReviewService(IGenericRepository<GameReview, Guid> reviewRepository) : IReviewService
     {
-        public Task<bool> ReviewAlreadyExistByGameIdAndUserIdAsync(string userId, string gameId)
+        private readonly IGenericRepository<GameReview, Guid> _reviewRepository = reviewRepository;
+
+        public async Task<bool> ReviewAlreadyExistByGameIdAndUserIdAsync(string userId, string gameId)
         {
-            throw new NotImplementedException();
+            GameReview? review = await _reviewRepository
+                .AllAsReadOnly()
+                .FirstOrDefaultAsync(r => r.GameId.ToString() == gameId && r.ReviewerId.ToString() == userId && r.IsDeleted == false);
+
+            if (review == null)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         public Task AddReviewAsync(string userId, string gameId)
