d91f60dc862f8742c8cf6e417645aed62613dd41
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-10 19:46:43 +0200

    Added IReviewService interface

---
+++ GameVerse/GameVerse.Services/Interfaces/IReviewService.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace GameVerse.Services.Interfaces
+{
+    internal interface IReviewService
+    {
+        Task<bool> ReviewAlreadyExistByGameIdAndUserIdAsync(string userId, string gameId);
+
+        Task AddReviewAsync(string userId, string gameId);
+
+        Task EditReviewAsync(string reviewId, string userId);
+
+        Task DeleteReviewAsync(string reviewId, string userId);
+    }
+}
