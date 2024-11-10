43c9a751b8149d4fbf094cfdf4d3f97d64226315
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-10 19:47:42 +0200

    Added Review Service

---
+++ GameVerse/GameVerse.Services/ReviewService.cs
@@ -0,0 +1,28 @@
+﻿
+using GameVerse.Services.Interfaces;
+
+namespace GameVerse.Services
+{
+    public class ReviewService : IReviewService
+    {
+        public Task<bool> ReviewAlreadyExistByGameIdAndUserIdAsync(string userId, string gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddReviewAsync(string userId, string gameId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditReviewAsync(string reviewId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteReviewAsync(string reviewId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
