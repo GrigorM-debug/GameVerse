709f6d299d5353df568d0a1724aebded8efa0738
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-14 18:36:49 +0200

    Added OnlyUserWithout roles custom authorization filter

---
+++ GameVerse/GameVerse.Web/Filters/OnlyUsersWithoutRolesAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace GameVerse.Web.Filters
+{
+    public class OnlyUsersWithoutRolesAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+                return;
+            }
+
+            bool isModerator = context.HttpContext.User.IsInRole("Moderator");
+            bool isAdmin = context.HttpContext.User.IsInRole("Admin");
+
+            if (isAdmin || isModerator)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status401Unauthorized);
+                return;
+            }
+        }
+    }
+}
