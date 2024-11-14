5be5954d63d83446406fdab84b7b0a06dc888052
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-14 02:58:38 +0200

    Added properties in ReviewDeleteView model

--- GameVerse/GameVerse.Web.ViewModels/Game/Review/ReviewDeleteViewModel.cs
+++ GameVerse/GameVerse.Web.ViewModels/Game/Review/ReviewDeleteViewModel.cs
@@ -4,5 +4,20 @@ namespace GameVerse.Web.ViewModels.Game.Review
 {
     public class ReviewDeleteViewModel
     {
+        public string Id { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+
+        public int Rating { get; set; }
+
+        public string ReviewerId { get; set; } = null!;
+
+        public string ReviewerName { get; set;} = null!;
+
+        public string CreatedOn { get; set; } = null!;
+
+        public string GameName { get; set; } = null!;
+
+        public string GameId { get; set; } = null!;
     }
 }
