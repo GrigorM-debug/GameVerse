cf128e3eca7128751b689d8c9804cc07a97f1243
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-15 22:33:53 +0200

    Added properties in GameCartItemsViewModel

--- GameVerse/GameVerse.Web.ViewModels/ShoppingCart/GameCartItemsViewModel.cs
+++ GameVerse/GameVerse.Web.ViewModels/ShoppingCart/GameCartItemsViewModel.cs
@@ -3,5 +3,16 @@ namespace GameVerse.Web.ViewModels.ShoppingCart
 {
     public class GameCartItemsViewModel
     {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        public string Price { get; set; } = null!;
+
+        public decimal TotalPrice { get; set; } 
+
+        public string AddedOn { get; set; } = null!;
     }
 }
