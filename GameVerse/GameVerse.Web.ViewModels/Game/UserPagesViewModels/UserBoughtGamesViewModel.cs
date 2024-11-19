f244cb8f1bb07b008e3d1c608ddcd1d54092526a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-19 23:26:46 +0200

    Added UserEventRegistrationsViewModel

--- GameVerse/GameVerse.Web.ViewModels/Game/UserPagesViewModels/UserBoughtGamesViewModel.cs
+++ GameVerse/GameVerse.Web.ViewModels/Game/UserPagesViewModels/UserBoughtGamesViewModel.cs
@@ -4,7 +4,7 @@ namespace GameVerse.Web.ViewModels.Game.UserPagesViewModels
 {
     public class UserBoughtGamesViewModel
     {
-        public string Id { get; set; } = null!;
+        public string GameId { get; set; } = null!;
 
         public string Title { get; set; } = null!;
 
---
+++ GameVerse/GameVerse.Web.ViewModels/Game/UserPagesViewModels/UserEventRegistrationsViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+namespace GameVerse.Web.ViewModels.Game.UserPagesViewModels
+{
+    public class UserEventRegistrationsViewModel
+    {
+        public string EventId { get; set; } = null!;
+
+        public string Topic { get; set; } = null!;
+
+        public string Image { get; set; } = null!;
+
+        public string RegistrationDate { get; set; } = null!;
+
+        public string TicketPrice { get; set; } = null!;
+
+        public int TicketQuantity { get; set; }
+
+    }
+}
