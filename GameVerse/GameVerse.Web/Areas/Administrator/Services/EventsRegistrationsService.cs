4fa42ee00321c26665977e621aa6b8617f3ad144
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 20:22:39 +0200

    Added EventsRegistrations service

---
+++ GameVerse/GameVerse.Web/Areas/Administrator/Services/EventsRegistrationsService.cs
@@ -0,0 +1,23 @@
+﻿using GameVerse.Data.Models.Events;
+using GameVerse.Data.Repositories.Interfaces;
+using GameVerse.Web.Areas.Administrator.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameVerse.Web.Areas.Administrator.Services
+{
+    public class EventsRegistrationsService(
+        IGenericRepository<EventRegistration, object> eventsRegistrationsRepository
+        ) : IEventsRegistrationsService
+    {
+        private readonly IGenericRepository<EventRegistration, object> _eventsRegistrationsRepository = eventsRegistrationsRepository;
+
+        public async Task<int> GetTotalEventsRegistrationsCountAsync()
+        {
+            int eventsRegistrationsTotalCount = await _eventsRegistrationsRepository
+                .AllAsReadOnly()
+                .CountAsync();
+
+            return eventsRegistrationsTotalCount;
+        }
+    }
+}
