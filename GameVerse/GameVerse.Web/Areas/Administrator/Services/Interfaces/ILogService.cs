46197f3e61d38fa008fefcc5b502c65d57c0fdc4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 19:52:44 +0200

    Added ILogService interface

---
+++ GameVerse/GameVerse.Web/Areas/Administrator/Services/Interfaces/ILogService.cs
@@ -0,0 +1,11 @@
+﻿namespace GameVerse.Web.Areas.Administrator.Services.Interfaces
+{
+    public interface ILogService
+    {
+        public (byte[] FileContent, string ContentType, string FileName)? GetLogFile(string fileName);
+
+        public bool LogFileExists(string fileName);
+
+        List<string> GetLatestLogFiles(int numberOfFiles);
+    }
+}
