60ae94c8c49d2d71551857d599478259348a6a68
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 21:44:52 +0200

    Added XML comments documentation in IEventsRegistrationsService Interface

--- GameVerse/GameVerse.Web/Areas/Administrator/Services/Interfaces/IEventsRegistrationsService.cs
+++ GameVerse/GameVerse.Web/Areas/Administrator/Services/Interfaces/IEventsRegistrationsService.cs
@@ -1,7 +1,17 @@
 ﻿namespace GameVerse.Web.Areas.Administrator.Services.Interfaces
 {
+    /// <summary>
+    /// Provides methods for managing and retrieving information about event registrations.
+    /// </summary>
     public interface IEventsRegistrationsService
     {
+        /// <summary>
+        /// Retrieves the total count of event registrations asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> representing the asynchronous operation. 
+        /// The task result contains the total number of event registrations as an <see cref="int"/>.
+        /// </returns>
         Task<int> GetTotalEventsRegistrationsCountAsync();
     }
 }
