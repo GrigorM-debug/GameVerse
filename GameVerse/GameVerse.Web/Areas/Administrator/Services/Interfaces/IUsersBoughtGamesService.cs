757b56d1482bd27173702c0635072091746f612c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 21:45:31 +0200

    Added XML comments documentation in UsersBoughtGamesService interface

--- GameVerse/GameVerse.Web/Areas/Administrator/Services/Interfaces/IUsersBoughtGamesService.cs
+++ GameVerse/GameVerse.Web/Areas/Administrator/Services/Interfaces/IUsersBoughtGamesService.cs
@@ -1,7 +1,17 @@
 ﻿namespace GameVerse.Web.Areas.Administrator.Services.Interfaces
 {
+    /// <summary>
+    /// Provides methods for managing and retrieving information about games purchased by users.
+    /// </summary>
     public interface IUsersBoughtGamesService
     {
+        /// <summary>
+        /// Retrieves the total count of games purchased by users asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> representing the asynchronous operation. 
+        /// The task result contains the total number of purchased games as an <see cref="int"/>.
+        /// </returns>
         Task<int> GetUsersBoughtGamesCountAsync();
     }
 }
