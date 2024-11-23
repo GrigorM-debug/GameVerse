52d907bf863a316732134bde4d2e83ce5e0477fc
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 19:48:32 +0200

    Added AdminIndexViewModel

---
+++ GameVerse/GameVerse.Web/Areas/Administrator/Models/AdminIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace GameVerse.Web.Areas.Administrator.Models
+{
+    public class AdminIndexViewModel
+    {
+        public int TotalUsers { get; set; }
+        public int TotalModerators { get; set; }
+        public int TotalAdministrators { get; set; }
+        public int TotalEvents { get; set; }
+        public int TotalGames { get; set; }
+        public int TotalBoughtGames { get; set; }
+        public int TotalEventRegistrations { get; set; }
+        public List<string> RecentLogs { get; set; } = new List<string>();
+    }
+}
