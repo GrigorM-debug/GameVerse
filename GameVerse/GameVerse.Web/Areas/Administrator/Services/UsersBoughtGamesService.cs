f247aebf8be77487309496729bdb5473401cd98d
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 21:52:25 +0200

    Added XML comments documentation in UsersBoughtGames Service class

--- GameVerse/GameVerse.Web/Areas/Administrator/Services/UsersBoughtGamesService.cs
+++ GameVerse/GameVerse.Web/Areas/Administrator/Services/UsersBoughtGamesService.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameVerse.Web.Areas.Administrator.Services
 {
+    /// <summary>
+    /// Service for managing and retrieving data about games purchased by users.
+    /// </summary>
     public class UsersBoughtGamesService(
         IGenericRepository<UserBoughtGame, object> usersBoughtGamesRepository
         ) : IUsersBoughtGamesService
@@ -12,6 +15,13 @@ namespace GameVerse.Web.Areas.Administrator.Services
         private readonly IGenericRepository<UserBoughtGame, object> _usersBoughtGamesRepository =
             usersBoughtGamesRepository;
 
+        /// <summary>
+        /// Retrieves the total count of games purchased by users asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> representing the asynchronous operation. 
+        /// The task result contains the total number of purchased games as an <see cref="int"/>.
+        /// </returns>
         public async Task<int> GetUsersBoughtGamesCountAsync()
         {
             int usersBoughtGamesCount = await _usersBoughtGamesRepository
