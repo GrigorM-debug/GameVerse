45f7c436ad51b945a04594c5644b7838209e3634
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 21:53:00 +0200

    Added XML comments documentation in EventsRegistrationsService class

--- GameVerse/GameVerse.Web/Areas/Administrator/Services/EventsRegistrationsService.cs
+++ GameVerse/GameVerse.Web/Areas/Administrator/Services/EventsRegistrationsService.cs
@@ -5,12 +5,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameVerse.Web.Areas.Administrator.Services
 {
+    /// <summary>
+    /// Service for managing event registrations and retrieving related data.
+    /// </summary>
+    /// <param name="eventsRegistrationsRepository">
+    /// A repository for accessing event registration data.
+    /// </param>
     public class EventsRegistrationsService(
         IGenericRepository<EventRegistration, object> eventsRegistrationsRepository
         ) : IEventsRegistrationsService
     {
         private readonly IGenericRepository<EventRegistration, object> _eventsRegistrationsRepository = eventsRegistrationsRepository;
 
+        /// <summary>
+        /// Retrieves the total count of event registrations asynchronously.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="Task{TResult}"/> representing the asynchronous operation. 
+        /// The task result contains the total number of event registrations as an <see cref="int"/>.
+        /// </returns>
         public async Task<int> GetTotalEventsRegistrationsCountAsync()
         {
             int eventsRegistrationsTotalCount = await _eventsRegistrationsRepository
