984a75ec957dabe6d892e353f0769c6879f12acf
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-23 17:12:13 +0200

    Added UserViewModel

---
+++ GameVerse/GameVerse.Web/Areas/Administrator/Models/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace GameVerse.Web.Areas.Administrator.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public List<string> Roles { get; set; }
+
+        public int TotalEventsCreated { get; set; }
+
+        public int TotalGamesCreated { get; set; }
+    }
+}
