c8571dbb3ca8ffabe821e2c741b5c8d8c8153d47
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-24 21:00:09 +0200

    Added ModeratorIndexViewModel

---
+++ GameVerse/GameVerse.Web/Areas/Moderator/Models/ModeratorIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace GameVerse.Web.Areas.Moderator.Models
+{
+    public class ModeratorIndexViewModel
+    {
+        public int TotalGamesCreated { get; set; }
+
+        public int TotalEventsCreated { get; set; }
+
+        public IEnumerable<ModeratorGameIndexViewModel> Last5CreatedGames { get; set; } =
+            new HashSet<ModeratorGameIndexViewModel>();
+
+        public IEnumerable<ModeratorEventIndexViewModel> Last5CreatedEvents { get; set; } = new HashSet<ModeratorEventIndexViewModel>();
+    }
+}
