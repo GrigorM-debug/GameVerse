08870f896639552110c016418a1c288d84e0df65
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-24 20:59:44 +0200

    Added ModeratorGameIndexViewModel. I need to display last 5 games created by the current moderator

---
+++ GameVerse/GameVerse.Web/Areas/Moderator/Models/ModeratorGameIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace GameVerse.Web.Areas.Moderator.Models
+{
+    public class ModeratorGameIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string CreatedOn { get; set; } = null!;
+
+        public string Price { get; set; } = null!;
+
+        public int QuantityInStock { get; set; }
+    }
+}
