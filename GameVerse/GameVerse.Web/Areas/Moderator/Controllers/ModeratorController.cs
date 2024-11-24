fd1a5b3c76b99060d6625cd369a2e0d7a8a9d8c0
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-24 20:57:25 +0200

    Added ModeratorController with DashBoard Action

---
+++ GameVerse/GameVerse.Web/Areas/Moderator/Controllers/ModeratorController.cs
@@ -0,0 +1,29 @@
+﻿using GameVerse.Web.Areas.Moderator.Models;
+using GameVerse.Web.Areas.Moderator.Services.Interfaces;
+using GameVerse.Web.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameVerse.Web.Areas.Moderator.Controllers
+{
+    [Area("Moderator")]
+    [Authorize(Roles = "Moderator")]
+    public class ModeratorController(IModeratorService moderatorService) : Controller
+    {
+        private readonly IModeratorService _moderatorService = moderatorService;
+
+        [HttpGet]
+        public async Task<IActionResult> Dashboard()
+        {
+            ModeratorIndexViewModel model = new ModeratorIndexViewModel()
+            {
+                TotalEventsCreated = await _moderatorService.TotalEventsCreatedAsync(User.GetId()),
+                TotalGamesCreated = await _moderatorService.TotalGamesCreatedAsync(User.GetId()),
+                Last5CreatedEvents = await _moderatorService.GetLast5CreatedEventsAsync(User.GetId()),
+                Last5CreatedGames = await _moderatorService.GetLast5CreatedGamesAsync(User.GetId())
+            };
+
+            return View(model);
+        }
+    }
+}
