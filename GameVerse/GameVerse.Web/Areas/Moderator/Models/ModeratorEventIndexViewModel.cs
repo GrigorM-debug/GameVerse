7564ea52f00b67092b8348410b17b99e2d1c8ee6
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-24 20:58:54 +0200

    Added ModeratorEventIndexViewModel. I need it to display latest 5 events created by the current moderator

---
+++ GameVerse/GameVerse.Web/Areas/Moderator/Models/ModeratorEventIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace GameVerse.Web.Areas.Moderator.Models
+{
+    public class ModeratorEventIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Topic { get; set; } = null!;
+
+        public string StartDate { get; set; } = null!;
+
+        public string EndDate { get; set; } = null!;
+
+        public int Seats { get; set; }
+
+        public string TicketPrice { get; set; } = null!;
+    }
+}
