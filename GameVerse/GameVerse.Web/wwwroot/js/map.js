deab745aa3ce89fc15c3c7e2b33546efdbb77880
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-25 19:54:20 +0200

    Added new function in map js
    The logic in the function gets the user location and calculates the distance between the user Location and the Event location

--- GameVerse/GameVerse.Web/wwwroot/js/map.js
+++ GameVerse/GameVerse.Web/wwwroot/js/map.js
@@ -1,4 +1,6 @@
-﻿function initializeMap(defaultLatitute, defaultLongtitude) {
+﻿const { error } = require("jquery");
+
+function initializeMap(defaultLatitute, defaultLongtitude) {
     // Initialize the map
     let map = L.map('map').setView([defaultLatitude, defaultLongitude], 13); // Starting position (Sofia)
 
@@ -28,4 +30,59 @@
         document.getElementById('latitude').value = event.latlng.lat;
         document.getElementById('longitude').value = event.latlng.lng;
     });
+}
+
+function displayMapWithDetails(eventLat, eventLng) {
+    const map = L.map('map').setView([eventLat, eventLng], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    const eventMarker = L.marker([eventLat, eventLng]).addTo(map);
+    eventMarker.bindPopup("Event Location!").openPopup();
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const userLat = position.coords.latitude;
+            const userLng = position.coords.longitude;
+
+            const userMarker = L.marker([userLat, userLng]).addTo(map);
+            userMarker.bindPopup("You are here!").openPopup();
+
+            const userLocation = L.latLng(userLat, userLng);
+            const eventLocation = L.latLng(eventLat, eventLng);
+
+            const distanceInMeters = userLocation.distanceTo(eventLocation);
+
+            const distanceInKilometers = (distanceInMeters / 1000).toFixed(2);
+
+            let distanceText;
+
+            if (distanceInMeters < 1000) {
+                distanceText = `${Math.round(distanceInMeters)} meters`;
+            } else {
+                distanceText = `${distanceInKilometers} km`;
+            }
+
+            const latlngs = [
+                [userLat, userLng],
+                [eventLat, eventLng]
+            ];
+
+            const pointsList = [userLocation, eventLocation];
+
+            const polyline = L.polyline(pointsList, { color: 'blue', weight: 3 }).addTo(map);
+
+            map.fitBounds(polyline.getBounds());
+
+            L.popup()
+                .setLatLng(eventLocation)
+                .setContent(`Distance from you to the event: ${distanceText}`)
+                .openOn(map);
+        },
+        (error) => {
+            alert("Failed to get your location")
+        }
+    )
 }
