468f5620fbcad3b1bff464d295c10c0c2466ef4a
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-27 01:49:47 +0200

    Added AddQuntityInStockViewModel in Moderator Area

---
+++ GameVerse/GameVerse.Web/Areas/Moderator/Models/AddQuantityInStockInputViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static GameVerse.Common.ApplicationConstants;
+using static GameVerse.Common.ApplicationConstants.GameConstants;
+
+namespace GameVerse.Web.Areas.Moderator.Models
+{
+    public class AddQuantityInStockInputViewModel
+    {
+        public string GameId { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string CreatedOn { get; set; } = null!;
+
+        [Required]
+        [Range(QuantityInStockMinValue, QuantityInStockMaxValue, ErrorMessage = RangeErrorMessage)]
+        public int QuantityInStock { get; set; }
+    }
+}
