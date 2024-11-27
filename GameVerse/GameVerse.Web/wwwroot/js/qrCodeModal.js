244e8870ab1da333e22a288b03097b5d8852c2ba
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-27 19:50:53 +0200

    Added qrCodeModal.js file

---
+++ GameVerse/GameVerse.Web/wwwroot/js/qrCodeModal.js
@@ -0,0 +1,22 @@
+﻿let modal = document.getElementById('qrModal');
+let modalImg = document.getElementById('qrModalImg');
+let captionText = document.getElementById('caption');
+let span = document.getElementsByClassName("close")[0];
+let qrCoceImages = document.querySelectorAll('.qr-image');
+
+qrCoceImages.forEach((img) => {
+    img.addEventListener('click', () => {
+        modal.style.display = 'block';
+        modalImg.src = img.src;
+    });
+});
+
+span.onclick = () => {
+    modal.style.display = 'none';
+};
+
+window.onclick = (event) => {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
