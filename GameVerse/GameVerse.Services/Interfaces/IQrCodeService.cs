7ad32abd82f6a6af7441e9b6f96b263131e4d02c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-27 18:55:11 +0200

    Added QrCodeService

---
+++ GameVerse/GameVerse.Services/Interfaces/IQrCodeService.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace GameVerse.Services.Interfaces
+{
+    public interface IQrCodeService
+    {
+        public string GenerateQrCode(string data);
+    }
+}
---
+++ GameVerse/GameVerse.Services/QrCodeService.cs
@@ -0,0 +1,23 @@
+﻿
+
+using GameVerse.Services.Interfaces;
+using QRCoder;
+
+namespace GameVerse.Services
+{
+    public class QrCodeService :IQrCodeService
+    {
+        public string GenerateQrCode(string data)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+
+            Base64QRCode base64QrCode = new Base64QRCode(qrCodeData);
+
+            string qrCodeImageAsBase64 = base64QrCode.GetGraphic(20);
+
+            return qrCodeImageAsBase64;
+        }
+    }
+}
