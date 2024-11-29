d6c9908e502171113c244668fd2f71c38f7899d4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-11-30 00:09:49 +0200

    Added new actions and logic for them in ShoppingCart Controller

--- GameVerse/GameVerse.Web/Controllers/ShoppingCartController.cs
+++ GameVerse/GameVerse.Web/Controllers/ShoppingCartController.cs
@@ -207,7 +207,7 @@ namespace GameVerse.Web.Controllers
 
                 _notyf.Success("Items successfully purchased. You can see them in Bought Games and Event Registrations Pages");
 
-                return RedirectToAction("Index", "Home", new {area=""});
+                return RedirectToAction("Index", "ShoppingCart", new {area=""});
             }
             catch (InvalidOperationException ex)
             {
@@ -216,6 +216,80 @@ namespace GameVerse.Web.Controllers
 
                 return RedirectToAction("Index", "ShoppingCart", new {area = ""});
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IncreaseGameItemQuantity(string gameId)
+        {
+            string? userId = User.GetId();
+
+            if (userId == null)
+            {
+                _notyf.Warning("You don't have the permission to do this");
+                return Unauthorized();
+            }
+
+            Game? game = await _gameService.GetGameByIdAsync(gameId);
+
+            if (game == null)
+            {
+                _notyf.Error("Game doesn't exist");
+                return NotFound();
+            }
+
+            bool gameItemExistInTheShoppingCart =
+                await _shoppingCartService.GameItemExistInTheShoppingCart(gameId, userId);
+
+            if (!gameItemExistInTheShoppingCart)
+            {
+                _notyf.Error("Item doesn't exist in your shopping cart");
+                return RedirectToAction("Index", "ShoppingCart", new { area = "" });
+            }
+
+            await _shoppingCartService.IncreaseGameQuantityAsync(gameId, userId);
+
+            _notyf.Success("Game quantity increased successfully");
+
+            Log.Information("User with ID {UserId} perform {Action} in {Controller}", userId, nameof(IncreaseGameItemQuantity), nameof(ShoppingCartController));
+
+            return RedirectToAction("Index", "ShoppingCart", new { area = "" });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DecreaseGameItemQuantity(string gameId)
+        {
+            string? userId = User.GetId();
+
+            if (userId == null)
+            {
+                _notyf.Warning("You don't have the permission to do this");
+                return Unauthorized();
+            }
+
+            Game? game = await _gameService.GetGameByIdAsync(gameId);
+
+            if (game == null)
+            {
+                _notyf.Error("Game doesn't exist");
+                return NotFound();
+            }
+
+            bool gameItemExistInTheShoppingCart =
+                await _shoppingCartService.GameItemExistInTheShoppingCart(gameId, userId);
+
+            if (!gameItemExistInTheShoppingCart)
+            {
+                _notyf.Error("Item doesn't exist in your shopping cart");
+                return RedirectToAction("Index", "ShoppingCart", new { area = "" });
+            }
+
+            await _shoppingCartService.DecreaseGameQuantityAsync(gameId, userId);
+
+            _notyf.Success("Game quantity decreased successfully");
+
+            Log.Information("User with ID {UserId} perform {Action} in {Controller}", userId, nameof(DecreaseGameItemQuantity), nameof(ShoppingCartController));
+
+            return RedirectToAction("Index", "ShoppingCart", new { area = "" });
         }
     }
 }
