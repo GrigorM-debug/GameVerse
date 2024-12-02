0fdff9d80e1ec8831e8dfda562ce2e6bac7f1cfc
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-12-03 00:40:24 +0200

    Added new ViewModels in Administrator Area

---
+++ GameVerse/GameVerse.Web/Areas/Administrator/Models/DecodedDataViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GameVerse.Web.Areas.Administrator.Models
+{
+    public class DecodedDataViewModel
+    {
+        public string EventId { get; set; } 
+
+        public string UserId { get; set; } 
+
+        public DateTime Date { get; set; } 
+    }
+}
---
+++ GameVerse/GameVerse.Web/Areas/Administrator/Models/ValidateTicketRequestInputViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace GameVerse.Web.Areas.Administrator.Models
+{
+    public class ValidateTicketRequestInputViewModel
+    {
+        public string QrData { get; set; } = string.Empty;
+    }
+}
