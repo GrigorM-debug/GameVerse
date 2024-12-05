b3f7d2666b33c9e4bba3d926857f0fb76cc12629
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-12-05 16:29:17 +0200

    Added test class for GameService

---
+++ GameVerse/GameVerse.Services.Tests/GameServiceTests.cs
@@ -0,0 +1,141 @@
+﻿
+
+using GameVerse.Common.Enums;
+using GameVerse.Data;
+using GameVerse.Data.Models.ApplicationUsers;
+using GameVerse.Data.Models.Games;
+using GameVerse.Data.Models.Games.Genres;
+using GameVerse.Data.Models.Games.Platform;
+using GameVerse.Data.Models.Games.Restrictions;
+using GameVerse.Data.Repositories;
+using GameVerse.Data.Repositories.Interfaces;
+using GameVerse.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameVerse.Services.Tests
+{
+    public class GameServiceTests
+    {
+        private DbContextOptions<GameVerseDbContext> _dbContextOptions;
+        private GameVerseDbContext _dbContext;
+        private IGameService _gameService;
+        private IGenericRepository<Game, Guid> _gameRepository;
+        private IGenericRepository<Genre,  Guid> _genreRepository;
+        private IGenericRepository<Platform, Guid> _platformRepository;
+        private IGenericRepository<Restriction, Guid> _restrictionRepository;
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<GameVerseDbContext>()
+                .UseInMemoryDatabase("GameVerseInMemoryTest" + Guid.NewGuid().ToString())
+                .Options;
+
+            _dbContext = new GameVerseDbContext(_dbContextOptions);
+
+            await SeedDatabase();
+
+            _gameRepository = new GenericRepository<Game, Guid>(_dbContext);
+            _genreRepository = new GenericRepository<Genre, Guid>(_dbContext);
+            _platformRepository = new GenericRepository<Platform, Guid>(_dbContext);
+            _restrictionRepository = new GenericRepository<Restriction, Guid>(_dbContext);
+
+            _gameService = new GameService(
+                _gameRepository,
+                _genreRepository,
+                _platformRepository,
+                _restrictionRepository
+            );
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await _dbContext.Database.EnsureDeletedAsync();
+            await _dbContext.DisposeAsync();
+        }
+
+        private async Task SeedDatabase()
+        {
+            ApplicationUser user = new ApplicationUser()
+            {
+                Id = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+                UserName = "TestUser",
+                NormalizedUserName = "TESTUSER",
+                Email = "test@abv.bg",
+                NormalizedEmail = "test@abv.bg",
+                FirstName = "Test",
+                LastName = "Testov"
+            };
+
+            Moderator moderator = new Moderator()
+            {
+                Id = Guid.NewGuid(),
+                User = user,
+                UserId = user.Id,
+                TotalEventsCreated = 0,
+                TotalGamesCreated = 0,
+            };
+
+            Restriction restriction = new Restriction()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Restriction"
+            };
+
+            Genre genre = new Genre()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Genre"
+            };
+
+            Platform platform = new Platform()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Platform"
+            };
+
+            Game game = new Game()
+            {
+                Id = Guid.Parse("00000000-0000-0000-0000-000000000002"),
+                Title = "Test Game",
+                Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                PublishingStudio = "Test Studio",
+                YearPublished = 2010,
+                CreatedOn = DateTime.UtcNow,
+                Price = 20,
+                Image = "wdffffffffffffffffffffff",
+                QuantityInStock = 10,
+                Type = GameType.DigitalKey,
+                IsDeleted = false,
+                PublisherId = moderator.Id
+            };
+
+            game.GamesGenres.Add(new GameGenre()
+            {
+                Genre = genre,
+                Game = game,
+            });
+
+            game.GamesPlatforms.Add(new GamePlatform()
+            {
+                Game = game,
+                Platform = platform
+            });
+
+            game.GamesRestrictions.Add(new GameRestriction()
+            {
+                Game = game,
+                Restriction = restriction
+            });
+
+            await _dbContext.Users.AddAsync(user);
+            await _dbContext.Moderators.AddAsync(moderator);
+            await _dbContext.Restrictions.AddAsync(restriction);
+            await _dbContext.Genres.AddAsync(genre);
+            await _dbContext.Platforms.AddAsync(platform);
+            await _dbContext.Games.AddAsync(game);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
