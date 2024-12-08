bba507ce665d48128a6981f0d8169bd09bbfba9c
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-12-09 00:18:39 +0200

    Added UserEventRegistrationViewModel

---
+++ GameVerse/GameVerse.Web/Areas/Administrator/Models/UserEventRegistrationInfoViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace GameVerse.Web.Areas.Administrator.Models
+{
+    public class UserEventRegistrationInfoViewModel
+    {
+        public string UserName { get; set; } = null!;
+        public string FullName { get; set; } = null!;
+
+        public string EventTopic { get; set; } = null!;
+
+        public int NumberOfTickets { get; set; }
+
+        public string PricePaid { get; set; } = null!;
+
+        public string RegistrationDate { get; set; } = null!;
+
+        public string StartDate { get; set; } = null!;
+
+        public string EndDate { get; set; } = null!;
+    }
+}
