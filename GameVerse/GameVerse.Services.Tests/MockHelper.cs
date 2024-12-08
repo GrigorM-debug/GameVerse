eeaf746b46115f162cc85801ec73963823e204c4
Grigor Jivkov Marinov <114864279+GrigorMarinov@users.noreply.github.com>
2024-12-09 01:34:44 +0200

    Added Mock you helper class

---
+++ GameVerse/GameVerse.Services.Tests/MockHelper.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace GameVerse.Services.Tests
+{
+    public class MockHelper
+    {
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            return new Mock<UserManager<TUser>>(
+                store.Object, null, null, null, null, null, null, null, null
+            );
+        }
+    }
+}
